b60ed6a1405f7e83da3f0a7df1d8684cf85500bc
No Soy Anónimo <nosoyanonimo@airmail.cc>
2025-07-25 10:34:06 -0600

    VEamos que se puede hacer para solucionar el error

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,25 +2,27 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- fontsettings
-config.font_size = 13
+config.font_size = 11
 config.font = wezterm.font("JetBrains Mono")
 
 -- Colores
 
 config.colors = {
-    -- cursor_bg = "white",
+    -- cursor_bg = "#777777",
+    -- selection_bg = "#777777",
     -- cursor_border = "white",
 
 }
 
-config.color_scheme = "rose-pine"
+-- config.color_scheme = "rose-pine"
+config.color_scheme = "Solarized (dark) (terminal.sexy)"
 
 -- Ventanas
 
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.95
 config.window_padding = {
     left = 0,
     right = 0,
